457e160e5e82df55a8641727b212492aa05cf0d3
JakaV2 <106079586+JakaV2@users.noreply.github.com>
2022-05-31 22:57:06 +0700

    Update and rename plugins/creator.js to plugin/creator.js

--- plugin/creator.js
+++ plugin/creator.js
@@ -7,7 +7,7 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------- NOMOR
   let nowner = `${nomorown.split`@`[0]}@s.whatsapp.net`
   let teksnomor = `${htki} *OWNER* ${htka}
-• @${nomorown.split`@`[0]} •
+✦ @${nomorown.split`@`[0]} ✦
 ------- ${nameown} -------
 
 📮 *Note:*
@@ -20,26 +20,24 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-*ɴᴀᴍᴇ:* Letta
-*ᴀɢᴇ:* 13ᵗʰ
-*ᴄʟᴀss:* 7
-*sᴛᴀᴛᴜs:* single 
+${htjava} *Nama RP:* Jaka
+${htjava} *Nama RL:* Private
+${htjava} *Umur:* 16 Tahun
+${htjava} *Kelas:* 10 Smp
+${htjava} *Status:* Couple
 
-*ʙɪʀᴛʜᴅᴀʏ:* 5 Oct 2008
-*ᴀᴅᴅʀᴇss:* Indonesia, jawa barat, bogor
-
-*- - sᴋɪʟʟs: - -* 
+${htjava} *Ulang Tahun:* 20 Juni 2006
+${htjava} *Alamat:* Malaysia, Kuala lumpur
+${htjava} *Skills:* 
 > JavaScript [89.7%]
 > Python [13.4%]
-> CSS [5.1%]
 > Html [1.2%]
 
-📷 *Instagram:* ${sig}
-🐈 *Github:* ${sgh}
-🥏 *Whatsapp* wa.me/${nomorown}
-
+${htjava} *Instagram:* ${sig}
+${htjava} *Github:* ${sgh}
+•·––––––––––––––––––––––––––·•
 `
-  let teks = ' '
+  let teks = 'P - Pilih dibawah kak ! ⁄(⁄ ⁄•⁄-⁄•⁄ ⁄)⁄'
 const sections = [
    {
 	title: `${htjava} OWNER –––––––––·•`,
@@ -74,13 +72,13 @@ const listMessage = {
           conn.reply(m.chat, teksnomor, m, { contextInfo: { mentionedJid: [nowner] }})
             break
             case 'bio':
-          conn.sendHydrated(m.chat, teksbio, wm, ppown, "wa.me/" + nomorown, "💬 ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], m)
+          conn.sendHydrated(m.chat, teksbio, wm, ppown, sig, "📷 Instagram", nomorown, '🌹 Nomor', [[null, null], [null, null],[null,null]], m)
             break
             
           default:
-            return await conn.sendMessage(m.chat, listMessage, { quoted: m, contextInfo: { mentionedJid: [m.sender] }})
+            return await conn.sendMessage(m.chat, listMessage, m, { contextInfo: { mentionedJid: [m.sender] }})
         }
-    } else if (/aoaooaoaooaoa/i.test(command)) {
+    } else if (/enchant|enchan/i.test(command)) {
       const count = args[2] && args[2].length > 0 ? Math.min(99999999, Math.max(parseInt(args[2]), 1)) : !args[2] || args.length < 4 ? 1 :Math.min(1, count)
       switch (_type) {
         case 't':
